2670dac60a56963a03cd67f0d1936bdf31978959
2151050359TranNgocQui <165293084+2151050359TranNgocQui@users.noreply.github.com>
2024-05-12 19:28:04 +0700

    them ham dang nhap

--- 52_Phu_57_Qui_WEBDRIVER/Form1.cs
+++ 52_Phu_57_Qui_WEBDRIVER/Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Threading;
 
 namespace _52_Phu_57_Qui_WEBDRIVER
 {
     public partial class Form1 : Form
     {
+        private IWebDriver driver;
+        private const string webURL = "https://www.alphabooks.vn/";
+
+        //_57_Qui_52_Phu
         public Form1()
         {
             InitializeComponent();
+
+        }
+        //_57_Qui_52_Phu
+        private void navigate()
+        {
+            ChromeDriverService chrome = ChromeDriverService.CreateDefaultService();
+            chrome.HideCommandPromptWindow = true;
+            driver = new ChromeDriver(chrome);
+            driver.Navigate().GoToUrl(webURL);
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            navigate();
         }
     }
 }
