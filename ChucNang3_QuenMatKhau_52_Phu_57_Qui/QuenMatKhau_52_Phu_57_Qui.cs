755e6234123227b0f432ffdebf85c9dae96517eb
2151050359TranNgocQui <165293084+2151050359TranNgocQui@users.noreply.github.com>
2024-05-14 13:33:57 +0700

    Quen mat khau va doi mat khau

---
+++ ChucNang3_QuenMatKhau_52_Phu_57_Qui/QuenMatKhau_52_Phu_57_Qui.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace QuenMatKhau_52_Phu_57_Qui
+{
+    [TestClass]
+    public class QuenMatKhau_52_Phu_57_Qui
+    {
+        IWebDriver driver_52_Phu_57_Qui;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            driver_52_Phu_57_Qui = new ChromeDriver();
+        }
+
+        public void SetUp()
+        {
+            driver_52_Phu_57_Qui.Navigate().GoToUrl("https://www.alphabooks.vn/");
+
+        }
+
+        [TestMethod]
+        public void TC1_52_Phu_57_Qui()
+        {
+            SetUp();
+            Thread.Sleep(1000);
+            IWebElement Button_Human_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.XPath("/html/body/header/div/div/div[2]/div[1]/div[2]/div/div[2]/a/img"));
+            Button_Human_52_Phu_57_Qui.Click();
+            Thread.Sleep(2000);
+            IWebElement QuenMatKhau_VaoDay_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.CssSelector("body > main > div > div > div > div > div > div.left-col > div.group-login.group-log > p > a"));
+            QuenMatKhau_VaoDay_52_Phu_57_Qui.Click();
+            Thread.Sleep(2000);
+            IWebElement RecoverEmail_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.Id("recover-email"));
+            RecoverEmail_52_Phu_57_Qui.SendKeys("ngquiofficial@gmail.com");
+            Thread.Sleep(2000);
+            IWebElement Button_SendMail_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.XPath("/html/body/main/div/div/div/div/div/div[1]/div[2]/form/input[3]"));
+            Button_SendMail_52_Phu_57_Qui.Click();
+            Thread.Sleep(3000);
+            IWebElement QuenMatKhau_VaoDay2_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.XPath("/html/body/main/div/div/div/div/div/div[1]/div[1]/p/a"));
+            QuenMatKhau_VaoDay2_52_Phu_57_Qui.Click();
+            Thread.Sleep(5000);
+            driver_52_Phu_57_Qui.Quit();
+        }
+
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\ForgotPasswordData_52_Phu_57_Qui\ForgotPasswordData_52_Phu_57_Qui.csv", "ForgotPasswordData_52_Phu_57_Qui#csv", DataAccessMethod.Sequential)]
+        public void TC2_52_Phu_57_Qui()
+        {
+            var email = TestContext.DataRow[0].ToString(); //Email
+
+            SetUp();
+            Thread.Sleep(1000);
+            IWebElement Button_Human_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.XPath("/html/body/header/div/div/div[2]/div[1]/div[2]/div/div[2]/a/img"));
+            Button_Human_52_Phu_57_Qui.Click();
+            Thread.Sleep(2000);
+            IWebElement QuenMatKhau_VaoDay_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.CssSelector("body > main > div > div > div > div > div > div.left-col > div.group-login.group-log > p > a"));
+            QuenMatKhau_VaoDay_52_Phu_57_Qui.Click();
+            Thread.Sleep(2000);
+            IWebElement RecoverEmail_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.Id("recover-email"));
+            RecoverEmail_52_Phu_57_Qui.SendKeys(email);
+            Thread.Sleep(2000);
+            IWebElement Button_SendMail_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.XPath("/html/body/main/div/div/div/div/div/div[1]/div[2]/form/input[3]"));
+            Button_SendMail_52_Phu_57_Qui.Click();
+            Thread.Sleep(5000);
+            driver_52_Phu_57_Qui.Quit();
+
+        }
+
+    }
+}
---
+++ ChucNang4_DoiMatKhau_52_Phu_57_Qui/DoiMatKhau_52_Phu_57_Qui.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DoiMatKhau_52_Phu_57_Qui
+{
+    [TestClass]
+    public class DoiMatKhau_52_Phu_57_Qui
+    {
+        IWebDriver driver_52_Phu_57_Qui;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            driver_52_Phu_57_Qui = new ChromeDriver();
+        }
+
+        public void SetUp()
+        {
+            driver_52_Phu_57_Qui.Navigate().GoToUrl("https://www.alphabooks.vn/");
+
+        }
+        [TestMethod]
+        public void TC1_DoiMatKhauThanhCong_52_Phu_57_Qui()
+        {
+            SetUp();
+            Thread.Sleep(1000);
+            IWebElement Button_Human_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.XPath("/html/body/header/div/div/div[2]/div[1]/div[2]/div/div[2]/a/img"));
+            Button_Human_52_Phu_57_Qui.Click();
+            IWebElement Account_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.CssSelector("input[type='email']"));
+            Account_52_Phu_57_Qui.SendKeys("ngquiofficial@gmail.com");
+            IWebElement Password_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.Id("customer_password"));
+            Password_52_Phu_57_Qui.SendKeys("123456789aA");
+            IWebElement BtnLogIn_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.ClassName("btn-login"));
+            BtnLogIn_52_Phu_57_Qui.Click();
+            IWebElement BtnDoiMatKhau_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.LinkText("Đổi mật khẩu"));
+            BtnDoiMatKhau_52_Phu_57_Qui.Click();
+            IWebElement Oldpass_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.Name("OldPassword"));
+            Oldpass_52_Phu_57_Qui.SendKeys("123456789aA");
+            Thread.Sleep(1000);
+            IWebElement newPass_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.Id("changePass"));
+            newPass_52_Phu_57_Qui.SendKeys("123456789aA");
+            Thread.Sleep(1000);
+            IWebElement confirmPass_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.Id("confirmPass"));
+            confirmPass_52_Phu_57_Qui.SendKeys("123456789aA");
+            Thread.Sleep(1000);
+            IWebElement buttonDefault_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.ClassName("button-default"));
+            buttonDefault_52_Phu_57_Qui.Click();
+            Thread.Sleep(3000);
+            driver_52_Phu_57_Qui.Quit();
+        }
+
+
+
+        public TestContext TestContext { get; set; }
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @".\DoiMatKhauData_52_Phu_57_Qui\DoiMatKhauData_52_Phu_57_Qui.csv", "DoiMatKhauData_52_Phu_57_Qui#csv", DataAccessMethod.Sequential)]
+        public void TC2_DoiMatKhauThatBai_52_Phu_57_Qui()
+        {
+            var oldPass = TestContext.DataRow[0].ToString();
+            var newPass = TestContext.DataRow[1].ToString(); //Email
+            var confirmPass = TestContext.DataRow[2].ToString(); //Email
+
+
+            SetUp();
+            Thread.Sleep(1000);
+            IWebElement Button_Human_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.XPath("/html/body/header/div/div/div[2]/div[1]/div[2]/div/div[2]/a/img"));
+            Button_Human_52_Phu_57_Qui.Click();
+            IWebElement Account_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.CssSelector("input[type='email']"));
+            Account_52_Phu_57_Qui.SendKeys("ngquiofficial@gmail.com");
+            IWebElement Password_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.Id("customer_password"));
+            Password_52_Phu_57_Qui.SendKeys("123456789aA");
+            IWebElement BtnLogIn_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.ClassName("btn-login"));
+            BtnLogIn_52_Phu_57_Qui.Click();
+            IWebElement BtnDoiMatKhau_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.LinkText("Đổi mật khẩu"));
+            BtnDoiMatKhau_52_Phu_57_Qui.Click();
+            IWebElement Oldpass_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.Name("OldPassword"));
+            Oldpass_52_Phu_57_Qui.SendKeys(oldPass);
+            Thread.Sleep(1000);
+            IWebElement newPass_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.Id("changePass"));
+            newPass_52_Phu_57_Qui.SendKeys(newPass);
+            Thread.Sleep(1000);
+            IWebElement confirmPass_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.Id("confirmPass"));
+            confirmPass_52_Phu_57_Qui.SendKeys(confirmPass);
+            Thread.Sleep(1000);
+            IWebElement buttonDefault_52_Phu_57_Qui = driver_52_Phu_57_Qui.FindElement(By.ClassName("button-default"));
+            buttonDefault_52_Phu_57_Qui.Click();
+            Thread.Sleep(3000);
+            driver_52_Phu_57_Qui.Quit();
+        }
+
+        
+
+        
+
+    }
+}
